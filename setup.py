6b2530fd73b7fb99fd8ae9917bb86df9d64133e8
Grammy Jiang <grammy.jiang@gmail.com>
2019-02-10 00:27:30 +1100

    Limit the python version in .travis.yml

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Topic :: Terminals",
     ],
     cmdclass=versioneer.get_cmdclass(),
