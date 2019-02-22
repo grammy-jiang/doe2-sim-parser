32c97a7ce098140a49a98f106d2346ffa054f030
Grammy Jiang <grammy.jiang@gmail.com>
2019-02-22 12:41:41 +1100

    Fix logging in __main__.py

--- doe2_sim_parser/__main__.py
+++ doe2_sim_parser/__main__.py
@@ -1,5 +1,5 @@
-import logging
 import argparse
+import logging
 
 from doe2_sim_parser.parse_report_beps import parse_beps
 from doe2_sim_parser.parse_report_bepu import parse_bepu
@@ -9,6 +9,7 @@ from doe2_sim_parser.update_google_spreadsheet import update_report
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
 
 PARSERS = {"BEPS": parse_beps, "BEPU": parse_bepu, "ES-D": parse_es_d}
 
@@ -16,7 +17,8 @@ PARSERS = {"BEPS": parse_beps, "BEPU": parse_bepu, "ES-D": parse_es_d}
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Parse SIM reports, and upload the parsed reports to "
-                    "Google Spreadsheet.")
+        "Google Spreadsheet."
+    )
     parser.add_argument("-s", "--sim", help="the path to sim file")
 
     return parser.parse_args()
@@ -25,16 +27,22 @@ def parse_args():
 def main():
     args = parse_args()
 
-    logger.info('Receive sim: %s', args.sim)
+    logger.info("Receive sim: %s", args.sim)
 
     sim = split_sim(args.sim)
 
     for report in sim.normal_reports:
         if report.code in PARSERS:
-            logger.info('Start parsing the report %s', report.code + ' ' + report.name)
+            logger.info(
+                "Start parsing the sim report %s",
+                "{code} {name}".format(code=report.code, name=report.name),
+            )
             parsed_report = PARSERS[report.code](report.report)
 
-            logger.info('Start update the report %s on Google Spreadsheet.', report.code + ' ' + report.name)
+            logger.info(
+                "Start update the parsed sim report %s on Google Spreadsheet.",
+                "{code} {name}".format(code=report.code, name=report.name),
+            )
             update_report(report=report._replace(report=parsed_report))
 
 
